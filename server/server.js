d6872ec6347ab2419abcf19473050d64c02dd626
Laophy <56947136+Laophy@users.noreply.github.com>
2023-04-20 17:19:43 -0500

    try

--- server/server.js
+++ server/server.js
@@ -1,42 +1,16 @@
 import Express from 'express'
+
+import APIRouter from './api/apiRoutes.js'
 import cors from 'cors'
 import http from 'http'
 import { Server } from 'socket.io'
 
 // Local port
 const port = 3000
+
 const app = new Express()
 
 app.use(cors())
-app.use(Express.json())
-
-// Universal logging route
-app.use((req, res, next) => {
-  console.log(`${req.method} request at ${req.url}`)
-  next()
-})
-
-app.get('/healthcheck', async (_req, res, _next) => {
-  const healthcheck = {
-    uptime: process.uptime(),
-    message: 'OK',
-    timestamp: Date.now()
-  }
-  try {
-    res.send(healthcheck)
-  } catch (error) {
-    healthcheck.message = error
-    res.status(503).send()
-  }
-})
-
-app.get('/', (req, res) => {
-  res.status(200).send('Healthy code!')
-})
-
-app.listen(port, () => {
-  console.log('Health check server is now up on port: ' + port)
-})
 
 const server = http.createServer(app)
 
@@ -58,4 +32,22 @@ io.on('connection', (socket) => {
 
 server.listen(3001, () => {
   console.log('Socket Server is running! (backend)')
+})
+
+app.use(Express.json())
+
+// Universal logging route
+app.use((req, res, next) => {
+  console.log(`${req.method} request at ${req.url}`)
+  next()
+})
+
+// Attach the movie routes
+app.use('/data', APIRouter)
+
+// Final static file route
+app.use(Express.static('./public'))
+
+app.listen(port, () => {
+  console.log(`Listening on port http://localhost:${port}`)
 })
