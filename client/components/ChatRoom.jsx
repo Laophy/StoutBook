31527b90757979319cccaeb7374a8b267a9ec934
Laophy <56947136+Laophy@users.noreply.github.com>
2023-04-20 20:26:07 -0500

    revert

--- client/components/ChatRoom.jsx
+++ client/components/ChatRoom.jsx
@@ -5,40 +5,9 @@ import TextField from '@mui/material/TextField'
 import Grid from '@mui/material/Unstable_Grid2'
 
 // Client socket connection
-import { Manager } from 'socket.io-client'
+import io from 'socket.io-client'
 import ChatCard from './ChatCard'
-
-const manager = new Manager('https://laophy.com:3001', {
-  autoConnect: true
-})
-
-const socket = manager.socket('/')
-
-socket.on('connect', () => {
-  const engine = socket.io.engine
-  console.log(engine.transport.name) // in most cases, prints "polling"
-
-  engine.once('upgrade', () => {
-    // called when the transport is upgraded (i.e. from HTTP long-polling to WebSocket)
-    console.log(engine.transport.name) // in most cases, prints "websocket"
-  })
-
-  engine.on('packet', ({ type, data }) => {
-    // called for each packet received
-  })
-
-  engine.on('packetCreate', ({ type, data }) => {
-    // called for each packet sent
-  })
-
-  engine.on('drain', () => {
-    // called when the write buffer is drained
-  })
-
-  engine.on('close', (reason) => {
-    // called when the underlying connection is closed
-  })
-})
+const socket = io('https//laophy.com:3001')
 
 export default function ChatRoom (props) {
   const [message, setMessage] = React.useState('')
